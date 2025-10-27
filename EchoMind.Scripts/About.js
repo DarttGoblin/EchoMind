5c9e22cedaa57bfe2b05c31359f05e63cd10983d
DarttGoblin <yassine.bazgour@gmail.com>
2025-10-27 15:39:21 +0100

    Removed selects

--- EchoMind.Scripts/About.js
+++ EchoMind.Scripts/About.js
@@ -2,6 +2,6 @@ const benefit_container = document.querySelectorAll('.benefit-container');
 
 benefit_container.forEach(container => {
     container.onclick = function() {
-        window.open('https://api.isic-archive.com/collections/249/?page=1', '_blank');
+        window.open('', '_blank');
     }
 });
